caee0fbb3efb624ae4b9d27fd472dc93de8f4e99
rxs-michael <ruanxusong@thundersdata.com>
2020-05-08 09:46:21 +0800

    fix: 修改babel配置,完善readme

--- babel.config.js
+++ babel.config.js
@@ -5,17 +5,17 @@ module.exports = {
       'import',
       {
         libraryName: '@td-design/charts',
-        customName: (name) => {
+        customName: name => {
           if (name.indexOf('create') > -1) {
             return `@td-design/charts/es/utils/${name}`;
           }
           return `@td-design/charts/es/components/${name}`;
         },
-        style: (name) => {
+        style: name => {
           if (name.indexOf('create') > -1) {
             return false;
           }
-          return `${name}/style/index`;
+          return true;
         },
       },
     ],
